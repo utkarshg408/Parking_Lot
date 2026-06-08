e998a4cd047cff4ad4ced8c2d82ecaca5428e4b0
utkarshg408 <utkarshg408@gmail.com>
2026-06-08 11:24:55 +0530

    added searching opption in parking lot

--- internal/services/parkingservices.go
+++ internal/services/parkingservices.go
@@ -15,11 +15,11 @@ func (s *ParkingServices) createParkingLot(capacity int) {
 		Slots:    make([]models.Slot, capacity),
 	}
 	for i := 0; i < capacity; i++ {
-    s.parkinglot.Slots[i] = models.Slot{
-        Slotnum: i + 1,
-        Car: nil,
-    }
-}
+		s.parkinglot.Slots[i] = models.Slot{
+			Slotnum: i + 1,
+			Car:     nil,
+		}
+	}
 }
 
 func (s *ParkingServices) parkCar(Regnum string, Color string) int {
@@ -27,9 +27,9 @@ func (s *ParkingServices) parkCar(Regnum string, Color string) int {
 	if s.parkinglot == nil {
 		return -1
 	}
-	  // if slot available park the car and return slot number
+	// if slot available park the car and return slot number
 	for i := 0; i < s.parkinglot.Capacity; i++ {
-		 
+
 		if s.parkinglot.Slots[i].Car == nil {
 			s.parkinglot.Slots[i].Car = &models.Car{
 				RegNum: Regnum,
@@ -38,16 +38,97 @@ func (s *ParkingServices) parkCar(Regnum string, Color string) int {
 			return s.parkinglot.Slots[i].Slotnum
 		}
 	}
-	  return -1
+	return -1
 }
 
-func (s *ParkingServices) leavecar(Slotnum int) {
-	  if s.parkinglot == nil {
-		 fmt.Println("Parking lot not created")
-		 return 0
-	  }
-	   if Slotnum > s.parkinglot.Capacity || Slotnum <= 0{
-		  fmt.Println("Invalid Slot number")
-		  return 0
-	   }
+func (s *ParkingServices) leavecar(Slotnum int) int {
+	if s.parkinglot == nil {
+		fmt.Println("Parking lot not created")
+		return -1
+	}
+	if Slotnum > s.parkinglot.Capacity || Slotnum <= 0 {
+		fmt.Println("Invalid Slot number")
+		return -1
+	}
+	if s.parkinglot.Slots[Slotnum-1].Car == nil {
+		fmt.Println("Slot is already empty")
+		return -1
+
+	}
+	if s.parkinglot.Slots[Slotnum-1].Car != nil {
+		s.parkinglot.Slots[Slotnum-1].Car = nil
+		fmt.Println("Slot number ", Slotnum, "is free")
+	}
+	return 1
+}
+
+func (s *ParkingServices) status() {
+	// checking all the occupied slots and printing the details
+	if s.parkinglot == nil {
+		fmt.Println("Parking lot is not created")
+		return
+	}
+	//anycar is parked
+	fmt.Printf("Slot No.\tRegistration No\tColour\n")
+	for i := 0; i < s.parkinglot.Capacity; i++ {
+		if s.parkinglot.Slots[i].Car != nil {
+			fmt.Printf("%d\t%s\t%s\n", s.parkinglot.Slots[i].Slotnum, s.parkinglot.Slots[i].Car.RegNum, s.parkinglot.Slots[i].Car.Color)
+		}
+	}
+
+}
+
+func (s *ParkingServices) getRegNumByColor(Color string) {
+	//checking all the slots and printing the registration number of cars with given color
+	if s.parkinglot == nil {
+		fmt.Println("Parking lot is not created")
+		return
+	}
+	fmt.Printf("Registration numbers of cars with color %s:\n", Color)
+	var found bool
+	for i := 0; i < s.parkinglot.Capacity; i++ {
+		if s.parkinglot.Slots[i].Car != nil && s.parkinglot.Slots[i].Car.Color == Color {
+			fmt.Printf("%s\n", s.parkinglot.Slots[i].Car.RegNum)
+			found = true
+		}
+	}
+	if !found {
+		fmt.Println("Not Found")
+	}
+}
+
+func (s *ParkingServices) getSlotNumByColor(Color string) {
+	if s.parkinglot == nil {
+		fmt.Println("Parking lot is not created")
+		return
+	}
+	fmt.Printf("Slot numbers of cars with color %s:\n", Color)
+	var found bool
+	for i := 0; i < s.parkinglot.Capacity; i++ {
+		if s.parkinglot.Slots[i].Car != nil && s.parkinglot.Slots[i].Car.Color == Color {
+			fmt.Printf("%d\n", s.parkinglot.Slots[i].Slotnum)
+			found = true
+		}
+	}
+	if !found {
+		fmt.Println("Not Found")
+	}
+}
+
+func (s *ParkingServices) getSlotNumByRegNum(Regnum string) {
+	if s.parkinglot == nil {
+		fmt.Println("Parking lot is not created")
+		return
+	}
+	fmt.Printf("Slot number of car with registration number %s:\n", Regnum)
+	var found bool
+	for i := 0; i < s.parkinglot.Capacity; i++ {
+		if s.parkinglot.Slots[i].Car != nil && s.parkinglot.Slots[i].Car.RegNum == Regnum {
+			fmt.Printf("%d\n", s.parkinglot.Slots[i].Slotnum)
+			found = true
+		}
+	}
+	if !found {
+		fmt.Println("Not Found")
+	}
 }

54496d9b8238f71f67855c738a2c93eeee3ae8db
utkarshg408 <utkarshg408@gmail.com>
2026-06-09 13:54:03 +0530

    added main.go in the file

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,28 @@
+package main
 
+import (
+	"Parking_Lot/internal/parser"
+	"Parking_Lot/internal/services"
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	fmt.Println("Welcome to the Parking Lot")
+	fmt.Println("Start your journey by giving the command")
+	// taking input
+	services := &services.ParkingServices{}
+	parser := parser.CommandParser{
+		Services: services,
+	}
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		command := scanner.Text()
+		if command == "exit" {
+			break
+		}
+		parser.ParseCommand(command)
+	}
+
+}

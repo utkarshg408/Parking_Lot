ccb04c6e676920a0a440cfda7650aa8c66550b89
utkarshg408 <utkarshg408@gmail.com>
2026-06-09 19:23:57 +0530

    changes made in main.go

--- cmd/main.go
+++ cmd/main.go
@@ -11,17 +11,48 @@ import (
 func main() {
 	fmt.Println("Welcome to the Parking Lot")
 	fmt.Println("Start your journey by giving the command")
+
 	// taking input
 	services := &services.ParkingServices{}
 	parser := parser.CommandParser{
 		Services: services,
 	}
+	if len(os.Args) > 1 {
+		file, err := os.Open(os.Args[1])
+		if err != nil {
+			fmt.Println("Error Opening the file")
+		}
+		defer file.Close()
+		scanner := bufio.NewScanner(file)
+
+		for scanner.Scan() {
+			command := scanner.Text()
+			parser.ParseCommand(command)
+			if command == "exit" {
+				break
+			}
+		}
+		if err := scanner.Err(); err != nil {
+			fmt.Println("Error reading file:", err)
+		}
+		return
+	}
+
+	// Interactive Mode
+	fmt.Println("Start your journey by giving the command")
+	fmt.Println("Type 'exit' to quit")
+
 	scanner := bufio.NewScanner(os.Stdin)
+
 	for scanner.Scan() {
+
 		command := scanner.Text()
+
 		if command == "exit" {
+			fmt.Println("Exiting Parking Lot...")
 			break
 		}
+
 		parser.ParseCommand(command)
 	}
 
